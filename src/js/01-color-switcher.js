abcc98bdad769d6653b57cb3e7b8a94a4b047221
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-10-25 22:41:09 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    body: document.querySelector('body'),
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]')
+}
+
+refs.start.addEventListener('click',onStartSort)
+refs.stop.addEventListener('click', () => { 
+    clearInterval(timerId);
+    disabled = false})
+
+let timerId = null;
+let disabled = false
+
+function onStartSort() {
+    if (disabled) {
+        return
+    };
+    disabled = true;
+    timerId = setInterval(() => {
+    let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+     refs.body.setAttribute('style', `background-color: ${randomColor}`);
+  }, 1000); 
+}
+
+
 
